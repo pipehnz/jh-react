141cdb2309691ec27839715ab2f86e100f6ba746
Juan Miguel Henao Arias <henaoariasjm@gmail.com>
2021-12-27 13:10:33 -0500

    Update Home component for the new screens

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
-import i18next from 'i18next';
 
-import logo from './assets/logo.svg';
+import SignUp from '../SignUp';
+
 import styles from './styles.module.scss';
 
 function Home() {
   return (
-    <div className={styles.app}>
-      <header className={styles.appHeader}>
-        <img src={logo} className={styles.appLogo} alt={i18next.t('Home:logoAlt') as string} />
-        <p className={styles.text}>{i18next.t('Home:welcome')}</p>
-      </header>
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <SignUp />
+      </div>
     </div>
   );
 }
