e70b0732c94c7ed2be3f069967559e2f2be2ba38
Juan Miguel Henao Arias <henaoariasjm@gmail.com>
2022-01-05 15:50:14 -0500

    Add Navbar to Home component

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import SignUp from '../SignUp';
+import Navbar from './../../components/Navbar';
 
 import styles from './styles.module.scss';
 
 function Home() {
   return (
     <div className={styles.container}>
+      <Navbar />
       <div className={styles.wrapper}>
         <SignUp />
       </div>
