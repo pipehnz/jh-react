07939e7a3b2ce4c903b5860b3b36cbd58627a38a
Juan Miguel Henao Arias <henaoariasjm@gmail.com>
2022-01-12 12:45:01 -0500

    Refactor i18n keys

---
+++ src/constants/i18n.ts
@@ -0,0 +1,25 @@
+import i18next from 'i18next';
+
+i18next.addResources('es', 'Validations', {
+  required: 'Campo requerido',
+  email: 'El email no es válido',
+  password: 'La contraseña debe de tener mínimo 8 carácteres, al menos una letra y un número',
+  matchPassword: 'La contraseñas no coinciden'
+});
+
+i18next.addResources('en', 'Validations', {
+  required: 'Field required',
+  email: 'Entered value does not match email format',
+  password: 'Password must have a minimum of 8 characters, at least one letter and one number',
+  matchPassword: 'Passwords do not match'
+});
+
+i18next.addResources('es', 'Buttons', {
+  signUp: 'Registrarse',
+  login: 'Iniciar sesión'
+});
+
+i18next.addResources('en', 'Buttons', {
+  signUp: 'Sign Up',
+  login: 'Login'
+});
