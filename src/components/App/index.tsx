8593fb529f0c9af4ab67998a88ac27d6b31e5292
Juan Miguel Henao Arias <henaoariasjm@gmail.com>
2022-01-12 12:58:29 -0500

    Fix lint errors

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -8,7 +8,7 @@ const queryClient = new QueryClient();
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient} >
+    <QueryClientProvider client={queryClient}>
       <Home />
     </QueryClientProvider>
   );
