271a28268c5a1d60e555ad6d145072c6cbc111e3
Juan Miguel Henao Arias <henaoariasjm@gmail.com>
2022-01-12 12:45:09 -0500

    Button toggle language

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,20 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import i18n from '../../config/i18n';
 
 import styles from './styles.module.scss';
 
 function Navbar() {
+  const [isActive, setActive] = useState(false);
+
+  const toggleClass = () => {
+    setActive(!isActive);
+  };
+
   function handleClick(lng: string) {
     i18n.changeLanguage(lng);
+    toggleClass();
   }
 
   return (
     <div className={styles.container}>
-      <button type="button" className="btn-lang" onClick={() => handleClick('en')}>
+      <button
+        type="button"
+        className={`btn-lang ${isActive ? 'btn-lang-active' : ''}`}
+        onClick={() => handleClick('en')}
+      >
         EN
       </button>
-      <button type="button" className="btn-lang-es" onClick={() => handleClick('es')}>
+      <button
+        type="button"
+        className={`btn-lang ${isActive ? '' : 'btn-lang-active'}`}
+        onClick={() => handleClick('es')}
+      >
         ES
       </button>
     </div>
