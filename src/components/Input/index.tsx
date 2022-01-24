0b37755aac0e816b6d0e08f55ae7f0106cc47962
Juan Miguel Henao Arias <henaoariasjm@gmail.com>
2022-01-24 17:38:28 -0500

    Add new prop to Input component

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -6,13 +6,16 @@ interface Props extends HTMLProps<HTMLInputElement> {
   label: string;
   type: 'text' | 'email' | 'password';
   error: string | undefined;
+  idField: string;
 }
 
-function Input({ label, type, error, ...props }: Props, ref: Ref<HTMLInputElement>) {
+function Input({ label, type, error, idField, ...props }: Props, ref: Ref<HTMLInputElement>) {
   return (
     <div className={styles.inputContent}>
-      <label className={styles.formLabel}>{label}</label>
-      <input {...props} type={type} className={styles.formInput} ref={ref} />
+      <label className={styles.formLabel} htmlFor={idField}>
+        {label}
+      </label>
+      <input {...props} type={type} className={styles.formInput} ref={ref} id={idField} />
       {error && <small className={styles.formAlert}>{error}</small>}
     </div>
   );
