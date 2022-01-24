76c78bb86e32e417110ff3b467b3b939589411f7
Juan Miguel Henao Arias <henaoariasjm@gmail.com>
2022-01-24 12:58:04 -0500

    Refactor SignUp component

--- src/screens/SignUp/index.tsx
+++ src/screens/SignUp/index.tsx
@@ -1,22 +1,15 @@
 import React, { useState } from 'react';
-import { useForm } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
 import { useMutation } from 'react-query';
 
-import FormCard from '../../components/FormCard';
-import Input from '../../components/Input';
-import { User } from '../../utils/types';
-import { regexEmail, regexPassword } from '../../constants/forms';
-import { signUp } from '../../services/UsersService';
-import Loading from '../../components/Spinner/components/loading';
+import FormCard from 'components/FormCard';
+import SignUpForm from 'components/SignUpForm';
+import Loading from 'components/Spinner/components/loading';
+import Wrapper from 'components/Wrapper';
+import { signUp } from 'services/UsersService';
+import { User } from 'utils/types';
 
 function SignUp() {
-  const {
-    register,
-    handleSubmit,
-    getValues,
-    formState: { errors }
-  } = useForm<User>();
   const { t } = useTranslation();
   const [errorMsg, setErrorMsg] = useState('');
   const { isLoading, isError, mutate } = useMutation((data: User) => signUp(data), {
@@ -28,79 +21,25 @@ function SignUp() {
       setErrorMsg(t('ErrorRequest:network'));
     }
   });
-  const onSubmit = () => {
-    mutate(getValues());
+  const onSubmit = (user: User) => {
+    mutate(user);
   };
 
   return (
     <FormCard>
-      <div className="content-form">
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <Input
-            {...register('firstName', { required: t('Validations:required') })}
-            label={t('SignUp:firstName')}
-            type="text"
-            error={errors.firstName?.message}
-          />
-
-          <Input
-            {...register('lastName', { required: t('Validations:required') })}
-            label={t('SignUp:lastName')}
-            type="text"
-            error={errors.lastName?.message}
-          />
-          <Input
-            {...register('email', {
-              required: t('Validations:required'),
-              pattern: {
-                value: regexEmail,
-                message: t('Validations:email')
-              }
-            })}
-            label={t('SignUp:email')}
-            type="email"
-            error={errors.email?.message}
-          />
-          <Input
-            {...register('password', {
-              required: t('Validations:required'),
-              pattern: {
-                value: regexPassword,
-                message: t('Validations:password')
-              }
-            })}
-            label={t('SignUp:password')}
-            type="password"
-            error={errors.password?.message}
-          />
-          <Input
-            {...register('passwordConfirmation', {
-              required: t('Validations:required'),
-              validate: {
-                match: v => v === getValues().password || t('Validations:matchPassword')
-              }
-            })}
-            label={t('SignUp:passwordConfirmation')}
-            type="password"
-            error={errors.passwordConfirmation?.message}
-          />
-
-          {isError && <span className="form-alert">{errorMsg}</span>}
-
-          {isLoading && (
-            <div className="row full-width center">
-              <Loading name="circle" />
-            </div>
-          )}
-
-          <button className="btn-submit" type="submit">
-            {t('Buttons:signUp')}
-          </button>
-        </form>
-      </div>
+      <Wrapper>
+        <SignUpForm onSubmit={onSubmit} />
+      </Wrapper>
       <a className="btn-link" href=".">
         {t('Buttons:login')}
       </a>
+
+      {isError && <span className="form-alert">{errorMsg}</span>}
+      {isLoading && (
+        <div className="row full-width center">
+          <Loading name="circle" />
+        </div>
+      )}
     </FormCard>
   );
 }
