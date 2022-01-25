3243586bb50e045bc858fd06c5c7793443efafd3
Juan Miguel Henao Arias <henaoariasjm@gmail.com>
2022-01-25 00:37:59 -0500

    Create the new forms for Login in our app

--- src/constants/i18n.ts
+++ src/constants/i18n.ts
@@ -2,14 +2,14 @@ import i18next from 'i18next';
 
 i18next.addResources('es', 'Validations', {
   required: 'Campo requerido',
-  email: 'El email no es válido',
+  email: 'El formato del mail no es correcto',
   password: 'La contraseña debe de tener mínimo 8 carácteres, al menos una letra y un número',
   matchPassword: 'La contraseñas no coinciden'
 });
 
 i18next.addResources('en', 'Validations', {
   required: 'Field required',
-  email: 'Entered value does not match email format',
+  email: 'The e-mail format is not correct',
   password: 'Password must have a minimum of 8 characters, at least one letter and one number',
   matchPassword: 'Passwords do not match'
 });
@@ -25,9 +25,11 @@ i18next.addResources('en', 'Buttons', {
 });
 
 i18next.addResources('es', 'ErrorRequest', {
-  network: 'Se ha presentado un problema al ejecutar la solicitud'
+  network: 'Se ha presentado un problema al ejecutar la solicitud',
+  notFound: 'Credenciales incorrectas'
 });
 
 i18next.addResources('en', 'ErrorRequest', {
-  network: 'A problem occurred while executing the request'
+  network: 'A problem occurred while executing the request',
+  notFound: 'Incorrect credentials'
 });
--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,7 +1,49 @@
-import React from 'react';
+/* eslint-disable no-console */
+import React, { useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { useMutation } from 'react-query';
+
+import FormCard from 'components/FormCard';
+import LoginForm from 'components/LoginForm';
+import Loading from 'components/Spinner/components/loading';
+import Wrapper from 'components/Wrapper';
+import { signIn } from 'services/UsersService';
+import { Credential } from 'utils/types';
 
 function Login() {
-  return <div>Login works!</div>;
+  const { t } = useTranslation();
+  const [errorMsg, setErrorMsg] = useState('');
+  const { isLoading, isError, mutate } = useMutation((credential: Credential) => signIn(credential), {
+    onSuccess: res => {
+      console.log('access-token', res.headers!['access-token']);
+      console.log('uid', res.headers?.uid);
+      console.log('client', res.headers?.client);
+    },
+    onError: () => {
+      setErrorMsg(t('ErrorRequest:notFound'));
+    }
+  });
+  const onSubmit = (credential: Credential) => {
+    mutate(credential);
+  };
+
+  return (
+    <FormCard>
+      <Wrapper>
+        <LoginForm onSubmit={onSubmit} />
+      </Wrapper>
+      <a className="btn-link" href="/sign_up">
+        {t('Buttons:signUp')}
+      </a>
+
+      {isError && <span className="form-alert">{errorMsg}</span>}
+      {isLoading && (
+        <div className="row full-width center">
+          <Loading name="circle" />
+        </div>
+      )}
+    </FormCard>
+  );
 }
 
 export default Login;
