e95de2e51eabefe0c5a08a2c7c30cd156f767de0
Juan Miguel Henao Arias <henaoariasjm@gmail.com>
2022-01-24 23:47:12 -0500

    Support routes in app

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import SignUp from '../SignUp';
+import Login from '../Login';
 import Navbar from '../../components/Navbar';
 
 import styles from './styles.module.scss';
@@ -10,7 +12,16 @@ function Home() {
     <div className={styles.container}>
       <Navbar />
       <div className={styles.wrapper}>
-        <SignUp />
+        <Router>
+          <Switch>
+            <Route path="/sign_up">
+              <SignUp />
+            </Route>
+            <Route path="/">
+              <Login />
+            </Route>
+          </Switch>
+        </Router>
       </div>
     </div>
   );
