594c709e5c63fa32ee10da88a1373405f6cb416d
Juan Miguel Henao Arias <henaoariasjm@gmail.com>
2022-01-25 12:35:13 -0500

    Move Router of Home component to App component

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,15 +1,31 @@
 import React from 'react';
 import { QueryClientProvider, QueryClient } from 'react-query';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Home from 'screens/Home/index';
+import Login from 'screens/Login';
+import SignUp from 'screens/SignUp';
 
 import 'scss/application.scss';
-import Home from 'screens/Home/index';
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <Home />
+      <Router>
+        <Switch>
+          <Route path="/home">
+            <Home />
+          </Route>
+          <Route path="/sign_up">
+            <SignUp />
+          </Route>
+          <Route path="/">
+            <Login />
+          </Route>
+        </Switch>
+      </Router>
     </QueryClientProvider>
   );
 }
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import SignUp from '../SignUp';
-import Login from '../Login';
 import Navbar from '../../components/Navbar';
 
 import styles from './styles.module.scss';
@@ -11,18 +8,7 @@ function Home() {
   return (
     <div className={styles.container}>
       <Navbar />
-      <div className={styles.wrapper}>
-        <Router>
-          <Switch>
-            <Route path="/sign_up">
-              <SignUp />
-            </Route>
-            <Route path="/">
-              <Login />
-            </Route>
-          </Switch>
-        </Router>
-      </div>
+      <div className={styles.wrapper} />
     </div>
   );
 }
