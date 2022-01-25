ab66313b19faed796c79b1aecf811d8aca7965cb
Juan Miguel Henao Arias <henaoariasjm@gmail.com>
2022-01-25 13:52:17 -0500

    Navbar component layout

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
 
+import logoWolox from 'assets/LogoWolox.png';
+
+import styles from './styles.module.scss';
+
 function Navbar() {
-  return <></>;
+  return (
+    <>
+      <nav className={styles.navbar}>
+        <img className={styles.logo} src={logoWolox} alt="Logo Wolox" />
+        <button type="button" className="btn-logout">
+          Logout
+        </button>
+      </nav>
+    </>
+  );
 }
 
 export default Navbar;
