2128c35ad17527648235dcb27e97a37f3a59b6b6
Juan Miguel Henao Arias <henaoariasjm@gmail.com>
2022-01-25 00:29:58 -0500

    Add SignIn service

--- src/services/UsersService.ts
+++ src/services/UsersService.ts
@@ -1,4 +1,6 @@
 import api from '../config/api';
-import { User } from '../utils/types';
+import { User, Credential } from '../utils/types';
 
 export const signUp = (userPayload: User) => api.post('/users', userPayload);
+
+export const signIn = (crendentialPayload: Credential) => api.post('/users/sign_in', crendentialPayload);
