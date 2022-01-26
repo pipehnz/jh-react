7a8d0d7acc6acc24bad7126df52b3070b5184967
Juan Miguel Henao Arias <henaoariasjm@gmail.com>
2022-01-26 00:04:10 -0500

    Add call of BookList in Home screen

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 
+import BookList from '../../components/BookList';
 import Navbar from '../../components/Navbar';
 
 import styles from './styles.module.scss';
 
 function Home() {
   return (
-    <div className={styles.container}>
-      <Navbar />
-      <div className={styles.wrapper} />
-    </div>
+    <>
+      <div className={styles.container}>
+        <Navbar />
+        <div className={styles.wrapper}>
+          <BookList />
+        </div>
+      </div>
+    </>
   );
 }
 
