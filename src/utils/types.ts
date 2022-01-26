bd4ef01f058732595068f68ffa17185acee40600
Juan Miguel Henao Arias <henaoariasjm@gmail.com>
2022-01-25 23:55:48 -0500

    Create some types relate to new BookList screen

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -13,3 +13,24 @@ export type Credential = {
   email: string;
   password: string;
 };
+
+export type Book = {
+  id: number;
+  author: string;
+  title: string;
+  imageUrl: string;
+  editor: string;
+  year: string;
+  genre: string;
+  createdAt: Date;
+  updatedAt: Date;
+};
+
+export type BookListResponse = {
+  page: Book[];
+  count: number;
+  totalPages: number;
+  totalCount: number;
+  currentPage: number;
+  nextPage?: number;
+};
